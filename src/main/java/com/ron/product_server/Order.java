4f11ca1155d68fdb47259507f606ed44cee6d2f6
Aaron <arron0404@gmail.com>
2025-01-10 14:01:50 +0800

    Reapply "no message"
    This reverts commit e9304f0068b075d50b33a6b31cc5dc954dc087e4.

---
+++ src/main/java/com/ron/product_server/Order.java
@@ -0,0 +1,119 @@
+package com.ron.product_server;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Order {
+    private Integer ordNo;         // 訂單編號
+    private Integer memNo;         // 會員編號
+    private Integer prodNo;        // 產品編號
+    private String prodName;       // 產品名稱
+    private Double prodPrice;      // 產品價格
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") // 指定日期時間格式
+    private LocalDateTime ordDt;   // 訂單日期
+    private String cardNo;         // 信用卡號
+    private String expDate;        // 信用卡到期日
+    private String cvv;            // 信用卡 CVV
+    private Boolean isSubmitted;   // 是否已提交
+    
+    // 無參數構造函數
+    public Order() {
+    }
+
+    // 建構子
+    public Order(Integer ordNo, Integer memNo, Integer prodNo, String prodName, Double prodPrice, 
+                 LocalDateTime ordDt, String cardNo, String expDate, String cvv, Boolean isSubmitted) {
+        this.ordNo = ordNo;
+        this.memNo = memNo;
+        this.prodNo = prodNo;
+        this.prodName = prodName;
+        this.prodPrice = prodPrice;
+        this.ordDt = ordDt;
+        this.cardNo = cardNo;
+        this.expDate = expDate;
+        this.cvv = cvv;
+        this.isSubmitted = isSubmitted;
+    }
+
+    // Getter 和 Setter 方法
+    public Integer getOrdNo() { 
+        return ordNo; 
+    }
+
+    public void setOrdNo(Integer ordNo) { 
+        this.ordNo = ordNo; 
+    }
+
+    public Integer getMemNo() { 
+        return memNo; 
+    }
+
+    public void setMemNo(Integer memNo) { 
+        this.memNo = memNo; 
+    }
+
+    public Integer getProdNo() { 
+        return prodNo; 
+    }
+
+    public void setProdNo(Integer prodNo) { 
+        this.prodNo = prodNo; 
+    }
+
+    public String getProdName() { 
+        return prodName; 
+    }
+
+    public void setProdName(String prodName) { 
+        this.prodName = prodName; 
+    }
+
+    public Double getProdPrice() { 
+        return prodPrice; 
+    }
+
+    public void setProdPrice(Double prodPrice) { 
+        this.prodPrice = prodPrice; 
+    }
+
+    public LocalDateTime getOrdDt() { 
+        return ordDt; 
+    }
+
+    public void setOrdDt(LocalDateTime ordDt) { 
+        this.ordDt = ordDt; 
+    }
+
+    public String getCardNo() { 
+        return cardNo; 
+    }
+
+    public void setCardNo(String cardNo) { 
+        this.cardNo = cardNo; 
+    }
+
+    public String getExpDate() { 
+        return expDate; 
+    }
+
+    public void setExpDate(String expDate) { 
+        this.expDate = expDate; 
+    }
+
+    public String getCvv() { 
+        return cvv; 
+    }
+
+    public void setCvv(String cvv) { 
+        this.cvv = cvv; 
+    }
+
+    public Boolean getIsSubmitted() { 
+        return isSubmitted; 
+    }
+
+    public void setIsSubmitted(Boolean isSubmitted) { 
+        this.isSubmitted = isSubmitted; 
+    }
+}
---
+++ src/main/java/com/ron/product_server/Result.java
@@ -0,0 +1,77 @@
+package com.ron.product_server;
+
+public class Result {
+    private int code;        // 狀態碼
+    private String message;  // 訊息
+    private Order order;     // 包含完整訂單資料
+
+    // 只有訊息和狀態碼的建構函數
+    public Result(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    // 包含訂單物件的建構函數
+    public Result(int code, String message, Order order) {
+        this.code = code;
+        this.message = message;
+        this.order = order;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
+
+
+//public class Result {
+//	private int code;
+//	private String message = "";
+//
+//	public Result() {
+//		// TODO Auto-generated constructor stub
+//	}
+//	
+//	public Result(int code, String message) {
+//		super();
+//		this.code = code;
+//		this.message = message;
+//	}
+//
+//	public int getCode() {
+//		return code;
+//	}
+//
+//	public void setCode(int code) {
+//		this.code = code;
+//	}
+//
+//	public String getMessage() {
+//		return message;
+//	}
+//
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//
+//}
